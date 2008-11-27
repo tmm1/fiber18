8d521c966efd5b74768f839d59937a83d9ef185a
Aman <aman@tmm1.net>
2008-11-27 14:59:12 -0800

    use Thread.(critical|pass|stop) and Thread#(priority|wakeup) instead of Queue. first three tests pass, run a little bit quicker:
    $ rake
    (in /Users/test/fiber18)
    Loaded suite /usr/bin/rake
    Started
    ...
    Finished in 127.582703 seconds.
    
    3 tests, 7 assertions, 0 failures, 0 errors

--- lib/fiber18.rb
+++ lib/fiber18.rb
@@ -11,26 +11,36 @@ unless defined? Fiber
     def initialize
       raise ArgumentError, 'new Fiber requires a block' unless block_given?
 
-      @yield = Queue.new
-      @resume = Queue.new
+      @priority = (Thread.current[:fiber] and Thread.current[:fiber].priority + 1) || 1
+      @value = nil
 
-      @thread = Thread.new{ @yield.push [yield(*@resume.pop)] }
+      @thread = Thread.new(@priority){ |priority|
+        Thread.current.priority = priority
+        Thread.critical = false
+        Thread.stop
+        Thread.critical = true
+        Thread.current[:fiber].value = [ yield *Thread.current[:fiber].value ]
+      }
       @thread.abort_on_exception = true
       @thread[:fiber] = self
     end
-    attr_reader :thread
+    attr_reader :thread, :priority
+    attr_accessor :value
 
     def resume *args
       raise FiberError, 'dead fiber called' unless @thread.alive?
-      @resume.push(args)
-      result = @yield.pop
-      result.size > 1 ? result : result.first
+      @value = args
+      @thread.wakeup
+      Thread.pass
+      @value.size > 1 ? @value : @value.first
     end
 
     def yield *args
-      @yield.push(args)
-      result = @resume.pop
-      result.size > 1 ? result : result.first
+      @value = args
+      Thread.critical = false
+      Thread.stop
+      Thread.critical = true
+      @value.size > 1 ? @value : @value.first
     end
 
     def self.yield *args
@@ -57,6 +67,7 @@ unless defined? Fiber
   class RootFiber < Fiber
     def initialize
       # XXX: what is a root fiber anyway?
+      @priority = 1
     end
 
     def self.yield *args
